8ed68b9fc79d34e955fa729fd5363884e2e97317
giorgiopeng <84867579@qq.com>
2020-03-01 15:30:12 +0800

    模型加载

--- src/main/java/appState/EnemyState.java
+++ src/main/java/appState/EnemyState.java
@@ -4,12 +4,12 @@ import character.Role;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.BaseAppState;
-import com.jme3.material.Material;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
-import com.jme3.renderer.queue.RenderQueue;
-import com.jme3.scene.Geometry;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
-import com.jme3.scene.shape.Sphere;
+import com.jme3.scene.Spatial;
 
 import java.util.List;
 
@@ -20,13 +20,26 @@ public class EnemyState extends BaseAppState {
     @Override
     protected void initialize(Application application) {
         this.app = (SimpleApplication) getApplication();
-        Geometry grid = new Geometry("sphere",new Sphere(20,40,3));
-        Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.DarkGray);
-        grid.setMaterial(mat);
-        grid.setShadowMode(RenderQueue.ShadowMode.Off);
-        grid.move(4, 3, 0);
-        rootNode.attachChild(grid);
+        Spatial model = application.getAssetManager().loadModel("dragon2/dragon.obj");
+        model.scale(0.03f);// 按比例缩小
+        model.center();// 将模型的中心移到原点
+        model.move(7,0,0);
+        model.rotate(0,-1f,0);
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(2,1, -3));
+
+        // 环境光
+        AmbientLight ambient = new AmbientLight();
+
+        // 调整光照亮度
+        ColorRGBA lightColor = new ColorRGBA();
+        sun.setColor(lightColor.mult(0.8f));
+        ambient.setColor(lightColor.mult(1));
+
+        // #3 将模型和光源添加到场景图中
+        rootNode.addLight(sun);
+        rootNode.addLight(ambient);
+        rootNode.attachChild(model);
 
     }
 
